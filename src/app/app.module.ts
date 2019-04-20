68e7913aaf13224613adabccafd7217b999c4c20
tien anh <giakhang22102016@gmail.com>
2019-04-20 00:18:44 -0700

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RegisterPipe } from './register.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterPipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
