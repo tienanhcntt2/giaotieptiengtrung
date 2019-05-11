4b48943beb796e2cae6a4ecceebc09b5da7dbe4f
tien anh <giakhang22102016@gmail.com>
2019-05-11 01:17:45 -0700

    fix setting'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-  ngOnInit(): void {
-    throw new Error("Method not implemented.");
+export class HomePage implements OnInit {
+
+  //value title string
+  txt_level1: string;
+  txt_level2: string;
+  txt_level3: string;
+  txt_level4: string;
+  txt_vocabulary: string;
+  txt_setting: string;
+  txt_title : string;
+
+  lang: any;
+
+  // home page
+
+
+
+  constructor(public router: Router, public translate: TranslateService, public platform: Platform) {
+    if(localStorage.getItem("bg") === null){
+      localStorage.setItem("bg","#333333");
+    }
   }
+  ngOnInit(): void {   
+  }
+  ionViewWillEnter() {
+    this.lang = localStorage.getItem("lang");
+    this._initialiseTranslation();
+    this.translate.use(this.lang);
 
-  constructor(public router:Router){}
-
-  setting(){
+  }
+  setting() {
     this.router.navigateByUrl("setting");
   }
 
+  private _initialiseTranslation(): void {
+    setTimeout(() => {
+      this.txt_level1 = this.translate.instant("HOME.txt_level_1");
+      this.txt_level2 = this.translate.instant("HOME.txt_level_2");
+      this.txt_level3 = this.translate.instant("HOME.txt_level_3");
+      this.txt_level4 = this.translate.instant("HOME.txt_level_4");
+      this.txt_vocabulary = this.translate.instant("HOME.txt_vocabulary");
+      this.txt_setting = this.translate.instant("HOME.txt_setting");
+      this.txt_title = this.translate.instant("HOME.txt_title");
+    }, 250);
+  }
 }
--- src/app/setting/setting.page.ts
+++ src/app/setting/setting.page.ts
@@ -9,10 +9,29 @@ import { TranslateService } from '@ngx-translate/core';
 export class SettingPage implements OnInit {
 
   lang:any;
+  // value text title setting
   title: string;
+  txt_select_bg: string;
+  txt_title : string;
+  txt_pinjin: string;
+  txt_select_lg: string;
+  bg_content: any;
+  txt_version: string;
+  txt_share:string;
+  txt_review: string;
+  //value
+  pinjin : boolean;
+  version: string = "1.0.1";
+  // constructor
   constructor(public translate: TranslateService) {
+    this.bg_content = localStorage.getItem("bg");
+    if(localStorage.getItem("pinjin") === "true"){
+      this.pinjin = true;
+    }else{
+      this.pinjin = false;
+    }
     this.lang = localStorage.getItem("lang");
-    console.log("anhtt : "+ this.lang);
+
     this.translate.setDefaultLang(this.lang);
     this.translate.use(this.lang);
   }
@@ -23,15 +42,28 @@ export class SettingPage implements OnInit {
   switchLanguage() {
     this.translate.use(this.lang);
     localStorage.setItem("lang",this.lang);
-    console.log("anhtt : "+ this.lang);
+
     this._initialiseTranslation();
   }
   private _initialiseTranslation() : void
   {
      setTimeout(() =>
      {
-        this.title 			  = this.translate.instant("Setting.selectlanguage");
-        
+        this.txt_title  = this.translate.instant("Setting.title");
+        this.txt_select_bg = this.translate.instant("Setting.txt_select_bg");
+        this.txt_pinjin = this.translate.instant("Setting.txt_pinjin");
+        this.txt_select_lg = this.translate.instant("Setting.selectlanguage");
+        this.txt_version = this.translate.instant("Setting.txt_version");
+        this.txt_share = this.translate.instant("Setting.txt_share");
+        this.txt_review = this.translate.instant("Setting.txt_review");
      }, 250);
+  }
+  notify(){
+    localStorage.setItem("pinjin",""+this.pinjin);
+  }
+  selectBg(){
+    this.bg_content =this.bg_content;
+    localStorage.setItem("bg",this.bg_content);
+    
   }
 }
